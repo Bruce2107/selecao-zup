e0d99b8c29a87410181056d00abbd715f4bc38fd
Eduardo Henrique <edu.ed322@gmail.com>
2020-01-28 09:46:45 -0300

    Commit inicial

---
+++ src/main/kotlin/br/com/anibook/selecao/SelecaoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.anibook.selecao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SelecaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SelecaoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/anibook/selecao/SelecaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.anibook.selecao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelecaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
